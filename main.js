72af983a01e78e60a7336de1e13a9c60603b5b89
Itzel Annet <itzelmunozlove@gmail.com>
2025-04-09 15:03:44 -0600

    Se agregan los archivos del repositorio, el index ya hace su funcionamiento correctamente

---
+++ main.js
@@ -0,0 +1,13 @@
+let txtNombre = document.getElementById("txtNombre");
+let btnGuardar = document.getElementById("btnGuardar");
+
+
+
+btnGuardar.addEventListener("click", function(){
+    
+    let nombre = txtNombre.value.trim();
+    localStorage.setItem("nombre", nombre)
+    console.log("Nombre guardado");
+    
+    
+})
